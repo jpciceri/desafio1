3ad8b6c9aae94546c96e0215cf56ac811e96c7e0
Juan Pablo Ciceri <jpciceri@hotmail.com>
2023-07-11 20:30:01 -0300

    mejoras

--- primerDesafio.js
+++ primerDesafio.js
@@ -24,13 +24,17 @@ class ProductManager {
         }
     }
     getId() {
-        let max = 0;
+        const maxId = this.products.reduce((max,producto) => {
+            return producto.id > max ? producto.id : max;
+        }, 0);
 
-        this.products.forEach((producto) => {
-            max = producto.id > max ? producto.id : max
-        });
+        return maxId + 1
 
-        return max + 1
+        // this.products.forEach((producto) => {
+        //     max = producto.id > max ? producto.id : max
+        // });
+
+        // return max + 1
     }
 
     getProductById(id) {
@@ -50,6 +54,7 @@ newProduct.addProduct("TV Samsung 85", "SMART TV SAMSUNG 85” 8K NEOQLED", 2399
 newProduct.addProduct("TV LG 50", "SMART TV LG 50” 4K UHD THINQ AI", 169999,"https://www.naldo.com.ar/medias/Method-505299-1-515Wx515H?context=bWFzdGVyfGltYWdlc3w4NTY0MXxpbWFnZS9qcGVnfGltYWdlcy9oMGYvaDg5Lzk3MDQ1NTQyMzM4ODYuanBnfGM4MzEyZjI2MTg3MGUxYzUyMTc2YTBjNTI2MTY0YTZhYTFiZGRmZDEzNDVjMjY3ZTk3NzEzNjgwZDdkYjNlZWM", "TVLG50", 40)
 newProduct.addProduct("TV PHILIPS 55", "SMART TV PHILIPS 55 4K HDR ANDROID", 198000,"https://www.naldo.com.ar/medias/504799.jpg-515Wx515H?context=bWFzdGVyfHJvb3R8ODg0MDh8aW1hZ2UvanBlZ3xoMDkvaGJjLzk3MTUwODE5Njk2OTQuanBnfDYyZWM3NTE2MzI4MTI0YzliMzkwYzgxOWI5MDcyYzczNmU5YTQ3YWUxNDAwMTRmNTNkZjc2NzI2NWJiZTNjNjU", "TVPHILIPS55", 30)
 newProduct.addProduct("TV TCL 55", "SMART TV TCL 55” 4K GOOGLE TV", 179999,"https://www.naldo.com.ar/medias/Method-505163-1-515Wx515H?context=bWFzdGVyfGltYWdlc3wxNzY4NTN8aW1hZ2UvanBlZ3xpbWFnZXMvaGU5L2g3Mi85NjgxOTAyMjcyNTQyLmpwZ3wxZmE5MjhjZWVjNmVmY2M5NjQ5MjI2YmE4Zjc1ZjM0OGI3MDU1ZDJhMDRmMDJjZWQ1YjU3YTYxOTg4MzI5NTM3", "TVTCL55", 50)
+newProduct.addProduct("TV TCL 55", "SMART TV TCL 55” 4K GOOGLE TV", 179999,"https://www.naldo.com.ar/medias/Method-505163-1-515Wx515H?context=bWFzdGVyfGltYWdlc3wxNzY4NTN8aW1hZ2UvanBlZ3xpbWFnZXMvaGU5L2g3Mi85NjgxOTAyMjcyNTQyLmpwZ3wxZmE5MjhjZWVjNmVmY2M5NjQ5MjI2YmE4Zjc1ZjM0OGI3MDU1ZDJhMDRmMDJjZWQ1YjU3YTYxOTg4MzI5NTM3", "TVTCL55", 50)
 
 console.log(newProduct.getProducts())
-console.log(newProduct.getProductById(1))
+//console.log(newProduct.getProductById(1))
