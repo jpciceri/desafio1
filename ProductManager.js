4482380ec5d62aed3ff951bace7cadfd7407ad96
Juan Pablo Ciceri <jpciceri@hotmail.com>
2023-07-30 20:13:46 -0300

    Primera Pre-entrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,154 +1,110 @@
-// const fs = require("fs").promises;
-import { promises } from "fs";
+import fs from "fs";
 
 class ProductManager {
-
-    products;
-    id = 1;
-
     constructor() {
         this.products = [];
-        this.path = "./products.json";
+        this.path = "Products.json";
+        this.createFile();
     }
 
-    async loadData() {
-        this.products = await this.getProduct();
+    createFile() {
+        if (!fs.existsSync(this.path)) {
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
+        }
     }
-    async addProduct(product) {
-        const productValues = Object.values(product)
-        console.log("productValues", productValues)
 
-        productValues.forEach((value) => {
-            if (String(value).trim().length === 0) {
-                throw new Error("Completa todos los campos para continuar")
+    addProduct(product) {
+        if (this.validateCode(product.code)) {
+            console.log("Error! Code exists!");
+
+            return false;
+        } else {
+            const producto = {id:this.generateId(), title:product.title, description:product.description, code:product.code, price:product.price, status:product.status, stock:product.stock, category:product.category, thumbnails:product.thumbnails};
+            this.products = this.getProducts();
+            this.products.push(producto);
+            this.saveProducts();
+            console.log("Product added!");
+
+            return true;
+        }
+    }
+
+    updateProduct(id, product) {
+        this.products = this.getProducts();
+        let pos = this.products.findIndex(item => item.id === id);
+
+        if (pos > -1) {
+            this.products[pos].title = product.title;
+            this.products[pos].description = product.description;
+            this.products[pos].code = product.code;
+            this.products[pos].price = product.price;
+            this.products[pos].status = product.status;
+            this.products[pos].stock = product.stock;
+            this.products[pos].category = product.category;
+            this.products[pos].thumbnails = product.thumbnails;
+            this.saveProducts();
+            console.log("Product updated!");
+
+            return true;
+        } else {
+            console.log("Not found!");
+
+            return false;
+        }
+    }
+
+    deleteProduct(id) {
+        this.products = this.getProducts();
+        let pos = this.products.findIndex(item => item.id === id);
+
+        if (pos > -1) {
+            this.products.splice(pos, 1); (0,1)
+            this.saveProducts();
+            console.log("Product #" + id + " deleted!");
+
+            return true;
+        } else {
+            console.log("Not found!");
+
+            return false;
+        }
+    }
+
+    getProducts() {
+        let products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+
+        return products;
+    }
+
+    getProductById(id) {
+        this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+
+        return this.products.find(item => item.id === id) || "Not found";
+    }
+
+    validateCode(code) {
+        return this.products.some(item => item.code === code);
+    }
+
+    generateId() {
+        let max = 0;
+        let products = this.getProducts();
+
+        products.forEach(item => {
+            if (item.id > max) {
+                max = item.id;
             }
-            return value
-        })
+        });
 
-        const productCode = this.products.find(
-            (oneProduct) => oneProduct.code === product.code
-        )
-
-        if (productCode) {
-            throw new Error("El Code ya existe")
-        }
-
-        this.products.push({
-            ...product,
-            id: this.id
-        })
-        this.id = this.id + 1;
-
-        await promises.writeFile(this.path, JSON.stringify(this.products))
-        return "Su producto se creó con exito"
+        return max+1;
+        //return this.products.length > 0 ? this.products[this.products.length-1].id+1 : 1;
     }
 
-    async getProduct() {
-        try {
-            const products = await promises.readFile(this.path, {
-                encoding: "utf-8"
-            });
-            return JSON.parse(products);
-        } catch (error) {
-            console.log(`El archivo ${this.path} no existe, creando...`);
-            await promises.writeFile(this.path, "[]");
-            return [];
-        }
-    }
-
-    async getProductById(productId) {
-        try {
-            const products = await promises.readFile(this.path, {
-                encoding: "utf-8"
-            });
-            const productParsed = JSON.parse(products);
-
-            const productToFind = productParsed.find(
-                (product) => product.id === productId
-            );
-            return productToFind;
-        } catch (error) {
-            console.log(error);
-            throw new Error("No se encontro un producto con ese ID");
-        }
-    }
-
-    async updateProduct(id, product) {
-        const productToUpdate = await this.getProductById(id);
-        try {
-            if (productToUpdate) {
-                const filterProducts = this.products.filter((product) => product.id !== productToUpdate.id);
-                this.products.push([...filterProducts, Object.assign(productToUpdate, product)]);
-                await promises.writeFile(this.path, JSON.stringify(this.products));
-                return productToUpdate;
-            }
-        } catch (error) {
-            throw new Error("No se puedo actualizar el producto");
-        }
-    }
-
-    async deleteProduct(id) {
-        const productToDelete = await this.getProductById(id);
-        try {
-            const productsDeleted = this.products.filter(
-                (product) => product.id !== productToDelete.id
-            );
-            await promises.writeFile(this.path, JSON.stringify(productsDeleted));
-        } catch (error) {
-            throw new Error("No se pudo borrar el producto");
-        }
+    saveProducts() {
+        fs.writeFileSync(this.path, JSON.stringify(this.products));
     }
 }
 
-
-const main = async () => {
-const newProduct = new ProductManager()
-
-await newProduct.loadData()
-
-
-await newProduct.addProduct({
-    title:"TV Samsung 75",
-    description:"SMART TV SAMSUNG 75pulg CRYSTAL UHD 4K",
-    price: 450000,
-    thumbnail:"https://www.naldo.com.ar/medias/505102.1.jpg-515Wx515H?context=bWFzdGVyfHJvb3R8MzA0ODl8aW1hZ2UvanBlZ3xoODYvaDA4Lzk2Njc2NTg0ODE2OTQuanBnfDM0ZmE5ZWE2YmNjMjAzNThkYTI2OThlNzYwNDNlYWFkMmRiMjM0NjIwMTU2Y2RkZTFmZGI2OWZiNzMxNTA5YjY",
-    code: "TVSAMSUNG75",
-    stock: 20,})
-await newProduct.addProduct({
-    title:"TV Samsung 85",
-    description: "SMART TV SAMSUNG 85” 8K NEOQLED",
-    price: 2399999,
-    thumbnail: "https://www.naldo.com.ar/medias/QN800B-01-NUEVO.jpg-515Wx515H?context=bWFzdGVyfHJvb3R8MzYyODR8aW1hZ2UvanBlZ3xoNTcvaGRmLzk3MDEyMzcwMzA5NDIuanBnfDg4MmVhYWVkYzRkOWZhMTVlMDYwZDU5MGNjMWY1ZGU2NTI3MjlkNmQ1OWQwNmQzMGE0YWIzNTM2NTBkMGNhMDI",
-    code:"TVSAMSUNG85",
-    stock: 10,})
-await newProduct.addProduct({
-    title:"TV LG 50", 
-    description: "SMART TV LG 50” 4K UHD THINQ AI",
-    price: 169999, 
-    thumbnail:"https://www.naldo.com.ar/medias/Method-505299-1-515Wx515H?context=bWFzdGVyfGltYWdlc3w4NTY0MXxpbWFnZS9qcGVnfGltYWdlcy9oMGYvaDg5Lzk3MDQ1NTQyMzM4ODYuanBnfGM4MzEyZjI2MTg3MGUxYzUyMTc2YTBjNTI2MTY0YTZhYTFiZGRmZDEzNDVjMjY3ZTk3NzEzNjgwZDdkYjNlZWM",
-    code: "TVLG50",
-    stock: 40,})
-await newProduct.addProduct({
-    title:"TV PHILIPS 55",
-    description: "SMART TV PHILIPS 55 4K HDR ANDROID",
-    price: 198000,
-    thumbnail: "https://www.naldo.com.ar/medias/504799.jpg-515Wx515H?context=bWFzdGVyfHJvb3R8ODg0MDh8aW1hZ2UvanBlZ3xoMDkvaGJjLzk3MTUwODE5Njk2OTQuanBnfDYyZWM3NTE2MzI4MTI0YzliMzkwYzgxOWI5MDcyYzczNmU5YTQ3YWUxNDAwMTRmNTNkZjc2NzI2NWJiZTNjNjU",
-    code:"TVPHILIPS55", 
-    price:30,})
-await newProduct.addProduct({
-    title:"TV TCL 55",
-    description: "SMART TV TCL 55” 4K GOOGLE TV",
-    price: 179999,
-    thumbnail: "https://www.naldo.com.ar/medias/Method-505163-1-515Wx515H?context=bWFzdGVyfGltYWdlc3wxNzY4NTN8aW1hZ2UvanBlZ3xpbWFnZXMvaGU5L2g3Mi85NjgxOTAyMjcyNTQyLmpwZ3wxZmE5MjhjZWVjNmVmY2M5NjQ5MjI2YmE4Zjc1ZjM0OGI3MDU1ZDJhMDRmMDJjZWQ1YjU3YTYxOTg4MzI5NTM3", 
-    code:"TVTCL55",
-    price: 50,})
-
-
-    const products = await newProduct.getProduct()
-    console.log(products)
-
-}
-
-main();
+//module.exports = {ProductManager};
 
 export default ProductManager;
--- app.js
+++ app.js
@@ -1,119 +1,14 @@
 import express from "express";
-import ProductManager from "./ProductManager.js"
+import productRouter from "./src/routes/productRouter.js";
+import cartsRouter from "./src/routes/cartsRouter.js";
 
-const puerto = 8080
 const app = express();
-app.use(express.urlencoded({
-    extended: true
-}));
+const puerto = 8080;
+
 app.use(express.json());
-const productManagerServer = new ProductManager();
-
-const main = async () => {
-    await productManagerServer.addProduct({
-        title: "TV smart prueba1",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123atyyjyt",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba2",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123abqq",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba3",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123aertwc",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba4",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123abcuut",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba5",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123abklle",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba6",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123akkdef",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba7",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123abcghhj",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba8",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123abfhgfhf",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba9",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123adsaasgfg",
-        stock: "25",
-    });
-    await productManagerServer.addProduct({
-        title: "TV smart prueba10",
-        description: "Este es un producto prueba",
-        price: "200",
-        thumbnail: "Sin imagen",
-        code: "123eeefafss",
-        stock: "25",
-    });
-}
-
-main()
-
-
-app.get("/products", async (req, res) => {
-    let productsParsed = await productManagerServer.getProduct()
-    let limit = req.query.limit || 10;
-    const productsLimit = productsParsed.slice(0, limit);
-
-    return res.status(200).json(productsLimit)
-});
-
-app.get("/products/:pid", async (req, res) => {
-    const productId = +req.params.pid;
-    const productToFind = await productManagerServer.getProductById(productId);
-    if (!productToFind) return res.status(404).json({
-        message: "No existe ese producto"
-    })
-
-    return res.status(200).json(productToFind)
-});
+app.use("/api/products/", productRouter);
+app.use("/api/carts/", cartsRouter);
 
 app.listen(puerto, () => {
-    console.log(`Servidor escuchando en el puerto :${puerto}`);
+    console.log("Servidor activo en el puerto: " + puerto);
 });
-
